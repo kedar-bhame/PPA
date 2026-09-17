487ccd34fc1bea3982d2792ef7a9b1efc38fd1d1
KedarBhame01 <kedarbhame1999@gmail.com>
2026-09-17 23:39:49 +0530

    function calling
    1. by call by value

---
+++ C++_Programming/Practice/FunctionCalling1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Demo{
+    public :
+        int x, y ;
+        // Demo(){
+        //     cout<<"inside Default Constructor. \n";
+        //     this-> x = 0;
+        //     this-> y = 0;
+        // }
+        Demo(int a = 0, int b = 0)
+        {
+            cout<<"Inside Parameterised Constructor. \n";
+            this-> x = a;
+            this-> y = b;
+        }
+        Demo(Demo &ref){
+            cout<<"Inside copy Constructor. \n";
+            this-> x = ref.x;
+            this-> y = ref.y;
+        }
+};
+
+int main()
+{
+    class Demo dobj;
+    cout<<"x = "<<dobj.x<<"\n";
+    cout<<"y = "<<dobj.y<<"\n";
+
+    class Demo dobj2(11,21);
+    cout<<"x = "<<dobj2.x<<"\n";
+    cout<<"y = "<<dobj2.y<<"\n";
+
+    class Demo dobj3(dobj2);
+    cout<<"x = "<<dobj3.x<<"\n";
+    cout<<"y = "<<dobj3.y<<"\n";
+    
+    return 0;
+
+}
---
+++ Java_Programming/practice/Array.java
@@ -0,0 +1,7 @@
+class Array{
+    public static void main(String A[]){
+        int[][] Arr = new int [5][3];
+        Arr[0][5] = 10;
+        System.out.println(Arr[0]);
+    }
+}
